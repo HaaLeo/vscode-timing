aadae2c5b214d21cb15ebb4348ee9e5258a7f483
Leo Hanisch <hanisch@itq.lan>
2018-05-30 23:15:16 +0200

    Enable unit tests

--- src/test/unittest/timing.test.ts
+++ src/test/unittest/timing.test.ts
@@ -12,11 +12,11 @@ import * as vscode from 'vscode';
 import * as myExtension from '../../../src/timing';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite('Timing', () => {
+describe('Timing', () => {
 
     // Defines a Mocha unit test
-    test('Something 1', () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
+    it('Something 1', () => {
+        assert.equal(-1, [1, 2, 3].indexOf(9));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
 });

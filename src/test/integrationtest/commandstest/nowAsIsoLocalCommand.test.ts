6a8ac8c30ef6125e059f83e8ca98d07655e788af
unknown <hanisch_privat@itqlm033.itq.lan>
2018-06-12 00:12:33 +0200

    Added tests

--- src/test/integrationtest/commandstest/nowAsIsoLocalCommand.test.ts
+++ src/test/integrationtest/commandstest/nowAsIsoLocalCommand.test.ts
@@ -2,14 +2,14 @@
 
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import { NowAsCustomCommand } from '../../../commands/nowAsCustomCommand';
+import { NowAsIsoLocalCommand } from '../../../commands/nowAsIsoLocalCommand';
 import { DialogHandlerMock } from '../../mock/DialogHandlerMock';
 import { TimeConverterMock } from '../../mock/TimeConverterMock';
 
-describe('NowAsCustomCommand', () => {
+describe('NowAsIsoLocalCommand', () => {
     let dialogHandlerMock: DialogHandlerMock;
     let timeConverterMock: TimeConverterMock;
-    let testObject: NowAsCustomCommand;
+    let testObject: NowAsIsoLocalCommand;
 
     before(async () => {
         dialogHandlerMock = new DialogHandlerMock();
@@ -27,39 +27,29 @@ describe('NowAsCustomCommand', () => {
         beforeEach('Reset', () => {
             dialogHandlerMock.reset();
             timeConverterMock.reset();
-            testObject = new NowAsCustomCommand(timeConverterMock, dialogHandlerMock);
-            timeConverterMock.getNowAsCustom.returns('2018');
-        });
-
-        it('Should ask user to choose custom format', async () => {
-            dialogHandlerMock.showInputDialog.returns('YYYY');
-
-            await testObject.execute();
-
-            assert.equal(dialogHandlerMock.showInputDialog.calledOnce, true);
-            assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
-            assert.equal(dialogHandlerMock.showResultDialog.calledOnce, true);
-        });
-
-        it('Should stop if user canceled during custom format selection', async () => {
-            dialogHandlerMock.showInputDialog.returns(undefined);
-
-            await testObject.execute();
-
-            assert.equal(dialogHandlerMock.showInputDialog.calledOnce, true);
-            assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
-            assert.equal(dialogHandlerMock.showResultDialog.notCalled, true);
+            testObject = new NowAsIsoLocalCommand(timeConverterMock, dialogHandlerMock);
+            timeConverterMock.getNowAsIsoLocal.returns('1111');
         });
 
         it('Should show result after calculation', async () => {
-            dialogHandlerMock.showInputDialog.returns('YYYY');
-
             await testObject.execute();
 
-            assert.equal(dialogHandlerMock.showInputDialog.calledOnce, true);
+            assert.equal(dialogHandlerMock.showInputDialog.notCalled, true);
             assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
             assert.equal(dialogHandlerMock.showResultDialog.calledOnce, true);
-            assert.equal(timeConverterMock.getNowAsCustom.calledOnce, true);
+            assert.equal(timeConverterMock.getNowAsIsoLocal.calledOnce, true);
         });
+
+        it('Should update the time when hit enter again', async () => {
+            dialogHandlerMock.showResultDialog.onFirstCall().returns('1');
+            dialogHandlerMock.showResultDialog.onSecondCall().returns(undefined);
+            await testObject.execute();
+
+            assert.equal(dialogHandlerMock.showInputDialog.notCalled, true);
+            assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
+            assert.equal(dialogHandlerMock.showResultDialog.callCount, 2);
+            assert.equal(timeConverterMock.getNowAsIsoLocal.callCount, 2);
+        });
+
     });
 });
--- src/test/integrationtest/commandstest/nowAsIsoUtcCommand.test.ts
+++ src/test/integrationtest/commandstest/nowAsIsoUtcCommand.test.ts
@@ -2,14 +2,14 @@
 
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import { NowAsCustomCommand } from '../../../commands/nowAsCustomCommand';
+import { NowAsIsoUtcCommand } from '../../../commands/nowAsIsoUtcCommand';
 import { DialogHandlerMock } from '../../mock/DialogHandlerMock';
 import { TimeConverterMock } from '../../mock/TimeConverterMock';
 
-describe('NowAsCustomCommand', () => {
+describe('NowAsIsoUtcCommand', () => {
     let dialogHandlerMock: DialogHandlerMock;
     let timeConverterMock: TimeConverterMock;
-    let testObject: NowAsCustomCommand;
+    let testObject: NowAsIsoUtcCommand;
 
     before(async () => {
         dialogHandlerMock = new DialogHandlerMock();
@@ -27,39 +27,29 @@ describe('NowAsCustomCommand', () => {
         beforeEach('Reset', () => {
             dialogHandlerMock.reset();
             timeConverterMock.reset();
-            testObject = new NowAsCustomCommand(timeConverterMock, dialogHandlerMock);
-            timeConverterMock.getNowAsCustom.returns('2018');
-        });
-
-        it('Should ask user to choose custom format', async () => {
-            dialogHandlerMock.showInputDialog.returns('YYYY');
-
-            await testObject.execute();
-
-            assert.equal(dialogHandlerMock.showInputDialog.calledOnce, true);
-            assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
-            assert.equal(dialogHandlerMock.showResultDialog.calledOnce, true);
-        });
-
-        it('Should stop if user canceled during custom format selection', async () => {
-            dialogHandlerMock.showInputDialog.returns(undefined);
-
-            await testObject.execute();
-
-            assert.equal(dialogHandlerMock.showInputDialog.calledOnce, true);
-            assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
-            assert.equal(dialogHandlerMock.showResultDialog.notCalled, true);
+            testObject = new NowAsIsoUtcCommand(timeConverterMock, dialogHandlerMock);
+            timeConverterMock.getNowAsIsoUtc.returns('1111');
         });
 
         it('Should show result after calculation', async () => {
-            dialogHandlerMock.showInputDialog.returns('YYYY');
-
             await testObject.execute();
 
-            assert.equal(dialogHandlerMock.showInputDialog.calledOnce, true);
+            assert.equal(dialogHandlerMock.showInputDialog.notCalled, true);
             assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
             assert.equal(dialogHandlerMock.showResultDialog.calledOnce, true);
-            assert.equal(timeConverterMock.getNowAsCustom.calledOnce, true);
+            assert.equal(timeConverterMock.getNowAsIsoUtc.calledOnce, true);
         });
+
+        it('Should update the time when hit enter again', async () => {
+            dialogHandlerMock.showResultDialog.onFirstCall().returns('1');
+            dialogHandlerMock.showResultDialog.onSecondCall().returns(undefined);
+            await testObject.execute();
+
+            assert.equal(dialogHandlerMock.showInputDialog.notCalled, true);
+            assert.equal(dialogHandlerMock.showOptionsDialog.notCalled, true);
+            assert.equal(dialogHandlerMock.showResultDialog.callCount, 2);
+            assert.equal(timeConverterMock.getNowAsIsoUtc.callCount, 2);
+        });
+
     });
 });

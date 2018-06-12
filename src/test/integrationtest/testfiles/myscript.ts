b7fb0c522fb3e21874d88126d5f7ca3683bb85ea
HaaLeo <HanischLeo@freenet.de>
2018-06-12 09:38:53 +0200

    Renamed test variable

--- src/test/integrationtest/testfiles/myscript.ts
+++ src/test/integrationtest/testfiles/myscript.ts
@@ -4,4 +4,4 @@
 let unreadableEpochTimeString = 123456789;
 let unreadableUTCTimeString = '2018-06-03T10:32:57.000Z';
 let unreadableLocalTimeString = '1973-11-29T22:33:09.000+01:00';
-let unreadableCustomTimeString = '05/19/2018'
+let veryPrettyCustomTimeString = '05/19/2018'

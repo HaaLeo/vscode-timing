b974ae3741203d62ccfbe1c4e0ec979d2596dda0
HaaLeo <HanischLeo@freenet.de>
2018-06-14 16:23:02 +0200

    Test change to trigger travis

--- src/dialogHandler.ts
+++ src/dialogHandler.ts
@@ -24,6 +24,7 @@ class DialogHandler {
                         isValid = validateUserInput(input.originalInput);
                     }
 
+                    // test change TODO: remove
                     return isValid ? null : diagnoseMessage;
                 }
             }

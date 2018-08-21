7e2b30f50deea01da6f0d1df6dd44a4e83fa9c28
HaaLeo <HanischLeo@freenet.de>
2018-08-21 23:04:41 +0200

    Added further tests

--- src/test/steptest/InputBoxStep.test.ts
+++ src/test/steptest/InputBoxStep.test.ts
@@ -5,6 +5,8 @@ import * as sinon from 'sinon';
 import * as vscode from 'vscode';
 import { InputBoxStep } from '../../step/InputBoxStep';
 import { MultiStepHandler } from '../../step/MultiStepHandler';
+import { StepResult } from '../../step/StepResult';
+import { InputFlowAction } from '../../util/InputFlowAction';
 
 describe('InputBoxStep', () => {
     let testObject: InputBoxStep;
@@ -100,5 +102,94 @@ describe('InputBoxStep', () => {
             assert.strictEqual(inputBoxStub.totalSteps, 10);
         });
 
+        describe('validation succeeds', () => {
+
+            let listener: (e: any) => any;
+            let resultPromise: Thenable<StepResult>;
+            beforeEach(() => {
+                resultPromise = testObject.execute(new MultiStepHandler(), 0, 0);
+            });
+
+            afterEach(() => {
+                sinon.reset();
+            });
+
+            it('onDidAccept should resolve if input validation succeeded.', async () => {
+                inputBoxStub.value = '5555';
+                listener = inputBoxStub.onDidAccept.firstCall.args[0];
+                assert.strictEqual(inputBoxStub.onDidAccept.calledOnce, true);
+                const dummy: void = undefined;
+
+                listener(dummy);
+                const result = await resultPromise;
+
+                assert.strictEqual(result.value, '5555');
+                assert.strictEqual(result.action, InputFlowAction.Continue);
+            });
+
+            it('onDidChangeValue should take back validation message if input validation succeeded.', async () => {
+                listener = inputBoxStub.onDidChangeValue.firstCall.args[0];
+                assert.strictEqual(inputBoxStub.onDidChangeValue.calledOnce, true);
+
+                listener('not evaluated');
+
+                assert.strictEqual(inputBoxStub.validationMessage, '');
+            });
+
+            it('onDidHide should resolve to cancel action.', async () => {
+                listener = inputBoxStub.onDidHide.firstCall.args[0];
+                assert.strictEqual(inputBoxStub.onDidHide.calledOnce, true);
+                const dummy: void = undefined;
+
+                listener(dummy);
+                const result = await resultPromise;
+
+                assert.strictEqual(result.value, undefined);
+                assert.strictEqual(result.action, InputFlowAction.Cancel);
+            });
+        });
+
+        describe('validation fails', () => {
+            let listener: (e: any) => any;
+
+            beforeEach(() => {
+                spy.resetHistory();
+                testObject = new InputBoxStep(
+                    'test-placeholder',
+                    'test-prompt',
+                    'test-title',
+                    'test-validation-message',
+                    () => false);
+                assert.equal(spy.calledOnce, true);
+                const inputBox: vscode.InputBox = spy.returnValues[0];
+                inputBoxStub = sinon.stub(inputBox);
+                testObject.execute(new MultiStepHandler(), 0, 0);
+                assert.strictEqual(inputBoxStub.onDidAccept.calledOnce, true);
+                listener = inputBoxStub.onDidAccept.firstCall.args[0];
+            });
+
+            afterEach(() => {
+                sinon.reset();
+            });
+
+            it('onDidAccept should show the validation message if input validation fails.', () => {
+                assert.strictEqual(inputBoxStub.onDidAccept.calledOnce, true);
+                listener = inputBoxStub.onDidAccept.firstCall.args[0];
+                const dummy: void = undefined;
+
+                listener(dummy);
+
+                assert.strictEqual(inputBoxStub.validationMessage, 'test-validation-message');
+            });
+
+            it('onDidChange should show the validation message if input validation fails.', () => {
+                assert.strictEqual(inputBoxStub.onDidChangeValue.calledOnce, true);
+                listener = inputBoxStub.onDidChangeValue.firstCall.args[0];
+
+                listener('not evaluated');
+
+                assert.strictEqual(inputBoxStub.validationMessage, 'test-validation-message');
+            });
+        });
     });
 });

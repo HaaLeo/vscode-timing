fe4df9a6544293f671abb580a85972fe3f649f5c
HaaLeo <HanischLeo@freenet.de>
2018-09-08 17:58:34 +0200

    * Added copyright statement to each file * Added extension recommendations * Changed project eol to LF

--- src/util/resultBox.ts
+++ src/util/resultBox.ts
@@ -1,3 +1,10 @@
+/*!
+ * ---------------------------------------------------------------------------------------------
+ *  Copyright (c) Leo Hanisch. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ * --------------------------------------------------------------------------------------------
+ */
+
 'use strict';
 
 import { Disposable, ExtensionContext, InputBox, QuickInputButton, QuickInputButtons, Uri, window } from 'vscode';

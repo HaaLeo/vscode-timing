535892b66349e614321ecfdd571e293a79a81dde
Leo Hanisch <23164374+HaaLeo@users.noreply.github.com>
2019-12-31 19:31:57 +0100

    Replace tslint with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,204 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    "ignorePatterns": ["coverage/", "node_modules/", "dist/", "out/", "webpack.config.js"],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "import",
+        "prefer-arrow",
+        "@typescript-eslint",
+        "@typescript-eslint/tslint"
+    ],
+    "rules": {
+        "@typescript-eslint/adjacent-overload-signatures": "error",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/ban-types": "error",
+        "@typescript-eslint/class-name-casing": "error",
+        "@typescript-eslint/consistent-type-assertions": "error",
+        "@typescript-eslint/consistent-type-definitions": "error",
+        "@typescript-eslint/explicit-member-accessibility": [
+            "error",
+            {
+                "accessibility": "explicit"
+            }
+        ],
+        "@typescript-eslint/indent": [
+            "error",
+            4,
+            {
+                "FunctionDeclaration": {
+                    "parameters": "first"
+                },
+                "FunctionExpression": {
+                    "parameters": "first"
+                },
+                "SwitchCase": 1
+            }
+        ],
+        "@typescript-eslint/interface-name-prefix": "error",
+        "@typescript-eslint/member-delimiter-style": [
+            "error",
+            {
+                "multiline": {
+                    "delimiter": "semi",
+                    "requireLast": true
+                },
+                "singleline": {
+                    "delimiter": "semi",
+                    "requireLast": false
+                }
+            }
+        ],
+        "@typescript-eslint/member-ordering": "error",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-empty-interface": "error",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-misused-new": "error",
+        "@typescript-eslint/no-namespace": "error",
+        "@typescript-eslint/no-parameter-properties": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-var-requires": "error",
+        "@typescript-eslint/prefer-for-of": "error",
+        "@typescript-eslint/prefer-function-type": "error",
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/quotes": [
+            "error",
+            "single"
+        ],
+        "@typescript-eslint/semi": [
+            "error",
+            "always"
+        ],
+        "@typescript-eslint/triple-slash-reference": "error",
+        "@typescript-eslint/type-annotation-spacing": "error",
+        "@typescript-eslint/unified-signatures": "error",
+        "arrow-body-style": "error",
+        "arrow-parens": [
+            "error",
+            "as-needed"
+        ],
+        "camelcase": "error",
+        "comma-dangle": "off",
+        "complexity": "off",
+        "constructor-super": "error",
+        "curly": "error",
+        "dot-notation": "error",
+        "eol-last": "error",
+        "eqeqeq": [
+            "error",
+            "smart"
+        ],
+        "guard-for-in": "error",
+        "id-blacklist": "error",
+        "id-match": "error",
+        "import/order": "error",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "max-classes-per-file": [
+            "error",
+            1
+        ],
+        "max-len": "off",
+        "new-parens": "error",
+        "no-bitwise": "error",
+        "no-caller": "error",
+        "no-cond-assign": "error",
+        "no-console": "error",
+        "no-debugger": "error",
+        "no-empty": "off",
+        "no-eval": "error",
+        "no-fallthrough": "off",
+        "no-invalid-this": "off",
+        "no-multiple-empty-lines": "error",
+        "no-new-wrappers": "error",
+        "no-shadow": [
+            "error",
+            {
+                "hoist": "all"
+            }
+        ],
+        "no-throw-literal": "error",
+        "no-trailing-spaces": "error",
+        "no-undef-init": "error",
+        "no-underscore-dangle": "off",
+        "no-unsafe-finally": "error",
+        "no-unused-expressions": "error",
+        "no-unused-labels": "error",
+        "no-var": "error",
+        "object-shorthand": "error",
+        "one-var": [
+            "error",
+            "never"
+        ],
+        "prefer-arrow/prefer-arrow-functions": "error",
+        "prefer-const": "error",
+        "quote-props": [
+            "error",
+            "consistent-as-needed"
+        ],
+        "radix": "error",
+        "space-before-function-paren": [
+            "error",
+            {
+                "anonymous": "never",
+                "asyncArrow": "always",
+                "named": "never"
+            }
+        ],
+        "spaced-comment": [
+            "error",
+            "always", {
+                "block": {
+                    "markers": ["!"]
+                }
+            }
+        ],
+        "use-isnan": "error",
+        "valid-typeof": "off",
+        "@typescript-eslint/tslint/config": [
+            "error",
+            {
+                "rulesDirectory": ["node_modules/tslint/lib/rules"],
+                "rules": {
+                    "file-header": [
+                        true,
+                        "^!\n \\* ---------------------------------------------------------------------------------------------\n \\*  Copyright \\(c\\) Leo Hanisch. All rights reserved.\n \\*  Licensed under the MIT License. See LICENSE.txt in the project root for license information.\n \\* --------------------------------------------------------------------------------------------\n $",
+                        "---------------------------------------------------------------------------------------------\n Copyright (c) Leo Hanisch. All rights reserved.\n Licensed under the MIT License. See LICENSE.txt in the project root for license information.\n--------------------------------------------------------------------------------------------"
+                    ],
+                    "import-spacing": true,
+                    "jsdoc-format": true,
+                    "no-reference-import": true,
+                    "one-line": [
+                        true,
+                        "check-catch",
+                        "check-else",
+                        "check-finally",
+                        "check-open-brace",
+                        "check-whitespace"
+                    ],
+                    "whitespace": [
+                        true,
+                        "check-branch",
+                        "check-decl",
+                        "check-operator",
+                        "check-separator",
+                        "check-type",
+                        "check-typecast"
+                    ]
+                }
+            }
+        ]
+    }
+};
